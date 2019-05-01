5f13d6ec7ad78cb0c2f5bc7aae1b955d562332bc
Wojtek <wojciechtrusk@wp.pl>
2019-05-01 15:35:41 +0200

    generowanie szajsu

--- src/com/apw/MainForm.java
+++ src/com/apw/MainForm.java
@@ -1,4 +1,38 @@
 package com.apw;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MainForm {
+
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+    private JRadioButton radioButton4;
+    private JButton button1;
+    private JPanel mainPanel;
+    private JPanel radioPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+
+    public MainForm() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Telekorekcje");
+        frame.setContentPane(new MainForm().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
