c31738ceb67a7e3c1b59122fb32417b866c9916e
patryk246 <patryk.zamoscinski@gmail.com>
2019-05-02 18:43:49 +0200

    crc-16 i crc-12

--- src/com/apw/MainForm.java
+++ src/com/apw/MainForm.java
@@ -20,7 +20,14 @@ public class MainForm {
     private JPanel radioPanel;
     private JTextField komunikatField;
     private JTextField bitParzystosciField;
+    private JLabel bityPrzeklamaneLabel;
+    private JLabel daneAlgorytmuLabel;
+    private JLabel daneBinarneLabel;
+    private JLabel danePrzeklamaneLabel;
+    private JLabel daneTekstoweLabel;
     private int radioOption = 1;
+    private String crc12="1100000001111";
+    private String crc16="11000000000000101";
 
     public MainForm() {
 
@@ -30,12 +37,12 @@ public class MainForm {
                     parity();
                     break;
                 case 2:
-                    doAction(2);
-                    //todo dodać obsługę gdy jest wybrany 2 radio
+                    crcEncode(crc16);
+                    crcDecode(crc16);
                     break;
                 case 3:
-                    doAction(1);
-                    //todo dodać obsługę gdy jest wybrany 3 radio
+                    crcEncode(crc12);
+                    crcDecode(crc12);
                     break;
                 case 4:
                     doAction(1);
@@ -43,10 +50,10 @@ public class MainForm {
                     break;
             }
         });
-        radioButton1.addActionListener(e -> radioOption = 1);
-        radioButton2.addActionListener(e -> radioOption = 2);
-        radioButton3.addActionListener(e -> radioOption = 3);
-        radioButton4.addActionListener(e -> radioOption = 4);
+        radioButton1.addActionListener(e -> {radioOption = 1;customFieldNames();});
+        radioButton2.addActionListener(e -> {radioOption = 2;crcFieldNames();});
+        radioButton3.addActionListener(e -> {radioOption = 3;crcFieldNames();});
+        radioButton4.addActionListener(e -> {radioOption = 4;customFieldNames();});
         daneTekstoweField.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
@@ -163,4 +170,70 @@ public class MainForm {
         }
     }
 
+    //obliczenie sumy kontrolnej i dodanie jej do wiadomości
+    public void crcEncode(String crc){
+        String message=daneBinarneField.getText();
+        StringBuilder encoded=new StringBuilder(message);
+        for(int i=0;i<crc.length()-1;i++){
+            encoded.append('0');
+        }
+        for(int i=0;i<=encoded.length()-crc.length();){
+            for(int j=0;j<crc.length();j++){
+                //operacja modulo
+                if(encoded.charAt(i+j)==crc.charAt(j)){
+                    encoded.setCharAt(i+j, '0');
+                }
+                else{
+                    encoded.setCharAt(i+j, '1');
+                }
+            }
+            for(;i<encoded.length() && encoded.charAt(i)!='1';i++);
+        }
+        String finalMessage=message+encoded.toString().substring(encoded.length()-crc.length()+1);
+        bityPrzeklamaneField.setText(finalMessage);
+    }
+
+    //sprawdzenie poprawności wiadomości odebranej
+    public void crcDecode(String crc) {
+        String message = danePrzeklamaneField.getText();
+        if (message != null && !message.isEmpty()) {
+            boolean error = false;
+            StringBuilder encoded = new StringBuilder(message);
+            for (int i = 0; i <= encoded.length() - crc.length(); ) {
+                for (int j = 0; j < crc.length(); j++) {
+                    if (encoded.charAt(i + j) == crc.charAt(j)) {
+                        encoded.setCharAt(i + j, '0');
+                    } else {
+                        encoded.setCharAt(i + j, '1');
+                    }
+                }
+                for (; i < encoded.length() && encoded.charAt(i) != '1'; i++) ;
+            }
+            for (int i = encoded.length() - crc.length(); i < encoded.length(); i++) {
+                //jeśli po wykonaniu operacji modulo (na wiadomości odebranej) reszta jest różna od 0 wystąpił błąd
+                if (encoded.charAt(i) != '0') {
+                    error = true;
+                }
+            }
+            if (error) {
+                komunikatField.setText("Błąd w komunikacji");
+            } else {
+                komunikatField.setText("Wiadomość poprawna");
+            }
+        }
+    }
+
+    public void crcFieldNames(){
+        daneTekstoweLabel.setText("Wiadomość tekstowa");
+        daneBinarneLabel.setText("Wiadomość binarna");
+        bityPrzeklamaneLabel.setText("Wiadomość wysłana");
+        danePrzeklamaneLabel.setText("Wiadomość odebrana");
+    }
+
+    public void customFieldNames(){
+        daneTekstoweLabel.setText("Dane tekstowe");
+        daneBinarneLabel.setText("Dane binarne");
+        bityPrzeklamaneLabel.setText("Bity przekłamane");
+        danePrzeklamaneLabel.setText("Dane po przekłamaniu");
+    }
 }
